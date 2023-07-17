9b73118281b7799d8a4cdfc6f4b670b476e9e460
flukeez <flukelalalalal@gmail.com>
2023-07-17 10:04:20 +0700

    add database

--- dbworkspace.sql
+++ dbworkspace.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2023-07-14 16:16:15
+Date: 2023-07-17 10:01:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,15 +21,11 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `tbtypemanages`;
 CREATE TABLE `tbtypemanages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type_manage` varchar(256) NOT NULL,
+  `manage_type` varchar(256) NOT NULL,
   `durable_goods` int(11) NOT NULL,
   `building` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- ----------------------------
--- Records of tbtypemanages
--- ----------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- ----------------------------
 -- Table structure for tbtypemoneys
@@ -41,9 +37,4 @@ CREATE TABLE `tbtypemoneys` (
   `durable_goods` int(11) NOT NULL,
   `building` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- ----------------------------
--- Records of tbtypemoneys
--- ----------------------------
-INSERT INTO `tbtypemoneys` VALUES ('8', 'abc', '1', '2');
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
